951ba0765f19be4110e9c276801dc1a9eab2ac24
danielkot <daniodudis@gmail.com>
2021-04-19 11:15:35 +0200

    init

---
+++ lineage.mk
@@ -0,0 +1,15 @@
+include vendor/cm/config/common_full_phone.mk
+include device/sony/mt6757-common/mt6757.mk
+
+PRODUCT_RELEASE_NAME := Sony Xperia XA1 (Codename redwood)
+PRODUCT_DEVICE := redwood
+PRODUCT_NAME := lineage_redwood
+PRODUCT_BRAND := Sony
+PRODUCT_MODEL := Sony xperia xa1 (codename redwood)
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_GMS_CLIENTID_BASE := android-sony
+BUILD_FINGERPRINT := Sony/full_redwood/redwood:7.0/NRD90M/1504822148:user/test-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=redwood \
+    PRIVATE_BUILD_DESC="full_redwood-user 7.0 NRD90M 1504822148 test-keys"
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+for i in eng user userdebug; do
+	add_lunch_combo lineage_redwood-${i}
+done
